b4d9d9ab9b47f15b6194c67480ceb3108d475861
EC2 Default User <ec2-user@ip-172-31-21-65.ec2.internal>
2022-04-25 21:36:27 +0000

    フォロー機能実装完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   validates :title, presence: true
   validates :body, length: {in: 2...201}
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
   
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+     resource :relationships, only: [:create, :destroy]
+     get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end 
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
